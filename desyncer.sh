900ea36edbdade339a5347dd8a461c7371bf0300
Jeffu <jeffu@jeffu.tv>
2018-09-21 11:10:32 -0400

    Desyncer edit

--- desyncer.sh
+++ desyncer.sh
@@ -6,7 +6,9 @@
 #  Created by Jeffu Warmouth on 9/21/18.
 #
 
-sudo mkdir /media/usb
+if [!/media/usb]; then
+    sudo mkdir /media/usb
+fi
 sudo mount /dev/sda1 /media/usb
 cd /media/usb
 vid = $(find -name *.mp4)
